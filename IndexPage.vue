bd5c8e3b534498ffb79913bfc6a364dae70bf2fb
AmirReza <43963262+amirrezwa@users.noreply.github.com>
2022-11-18 12:19:56 +0330

    Add files via upload

---
+++ IndexPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn>Add new task</q-btn>
+    <button>Add new task</button>
+    <p>my favorite color<sup>blue</sup></p>
+    <q-input />
+    <input type="text" />
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+111
